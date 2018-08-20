33a743966efe4e4c26a7e451c12c23f23ab64f26
lxx <15890576644@163.com>
2018-08-20 15:35:20 +0800

    测试

---
+++ src/com/itstar/lxx/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.itstar.lxx;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        //策划i
+        System.out.println("HelloWorld");
+        for (String arg : args) {
+
+        }
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+        }
+        for (int i = 0; i < 10; i++) {
+
+        }
+        if (args != null) {
+
+        }
+        if (args == null) {
+
+        }
+        String xx="";
+        if (xx != null) {
+
+        }
+    }
+
+}
